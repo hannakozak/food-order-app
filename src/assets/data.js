47c927d533632b308edda20c0e5e3cd48245d5a7
Hanna Kozak <hannawkozak@gmail.com>
2021-10-24 22:12:48 +0100

    feat: add images to menu

--- src/assets/data.js
+++ src/assets/data.js
@@ -1,25 +1,31 @@
+import strawberry from './images/smoothie-strawberry.jpg';
+import workout from './images/smoothie-workout.jpg';
+import almond from './images/smoothie-almond.jpg';
+import healthy from './images/smoothie-healthy.jpg';
+
 export const data = [
     {
       id: 'm1',
       name: 'Super Strawberry Drink Co.',
-      description: 'Finest fish and veggies',
       price: 22.99,
+      image: strawberry,
     },
     {
       id: 'm2',
       name: 'Healthy Living Drink Co',
-      description: 'A german specialty!',
       price: 16.5,
+      image: healthy,
     },
     {
       id: 'm3',
       name: 'Workout Smoothie Bar',
-      description: 'American, raw, meaty',
       price: 12.99,
+      image: workout,
     },
     {
       id: 'm4',
       name: 'Almond Butter Shake Shop',
       price: 18.99,
+      image: almond,
     },
   ];
