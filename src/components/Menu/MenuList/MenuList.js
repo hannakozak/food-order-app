2706514b235ecbb1a42e7645124bace06a3657c0
Hanna Kozak <hannawkozak@gmail.com>
2022-04-02 23:19:43 +0100

    feat: move fetch menu data logic to useHttp hook

--- src/components/Menu/MenuList/MenuList.js
+++ src/components/Menu/MenuList/MenuList.js
@@ -1,30 +1,45 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useHttp } from "../../../hooks/useHttp";
 
 import MenuItem from "../MenuItem/MenuItem";
 import { ListWrapper } from "./MenuList.styles";
 
 const MenuList = () => {
   const [menu, setMenu] = useState([]);
+  const { isLoading, error, sendRequest: fetchMenu } = useHttp();
+
   useEffect(() => {
-    const fetchMenu = async () => {
-      const response = await fetch(
-        "https://food-order-app-ab838-default-rtdb.europe-west1.firebasedatabase.app/menu.json"
-      );
-      const responseData = await response.json();
+    const transformedMenu = (menuObj) => {
       const loadedMenu = [];
-      for (const key in responseData) {
+      for (const key in menuObj) {
         loadedMenu.push({
           id: key,
-          name: responseData[key].name,
-          price: responseData[key].price,
-          image: responseData[key].image,
+          name: menuObj[key].name,
+          price: menuObj[key].price,
+          image: menuObj[key].image,
         });
       }
       setMenu(loadedMenu);
-      console.log(loadedMenu);
     };
-    fetchMenu();
-  }, []);
+    fetchMenu(
+      {
+        url: "https://food-order-app-ab838-default-rtdb.europe-west1.firebasedatabase.app/menu.json",
+      },
+      transformedMenu
+    );
+  }, [fetchMenu]);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading ...</p>
+      </section>
+    );
+  }
+
+  if (error) {
+    return <section>{error}</section>;
+  }
   const menuList = menu.map((item) => (
     <MenuItem
       key={item.id}
